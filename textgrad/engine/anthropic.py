41068923f4739fa9c06ef79ac43bb39568ea8b27
dragonflower <marcoc@dragonflower.ai>
2024-10-15 10:45:11 +0200

    tools anthropic

--- textgrad/engine/anthropic.py
+++ textgrad/engine/anthropic.py
@@ -1,7 +1,10 @@
 try:
     from anthropic import Anthropic
+    from anthropic._types import NOT_GIVEN
 except ImportError:
-    raise ImportError("If you'd like to use Anthropic models, please install the anthropic package by running `pip install anthropic`, and add 'ANTHROPIC_API_KEY' to your environment variables.")
+    raise ImportError(
+        "If you'd like to use Anthropic models, please install the anthropic package by running `pip install anthropic`, and add 'ANTHROPIC_API_KEY' to your environment variables."
+    )
 
 import os
 import platformdirs
@@ -16,21 +19,26 @@ from typing import List, Union
 from .base import EngineLM, CachedEngine
 from .engine_utils import get_image_type_from_bytes
 
+
 class ChatAnthropic(EngineLM, CachedEngine):
     SYSTEM_PROMPT = "You are a helpful, creative, and smart assistant."
 
     def __init__(
         self,
-        model_string: str="claude-3-opus-20240229",
-        system_prompt: str=SYSTEM_PROMPT,
-        is_multimodal: bool=False,
+        model_string: str = "claude-3-opus-20240229",
+        system_prompt: str = SYSTEM_PROMPT,
+        is_multimodal: bool = False,
+        tools: list = NOT_GIVEN,
+        tool_choice: dict = NOT_GIVEN,
     ):
         root = platformdirs.user_cache_dir("textgrad")
         cache_path = os.path.join(root, f"cache_anthropic_{model_string}.db")
         super().__init__(cache_path=cache_path)
         if os.getenv("ANTHROPIC_API_KEY") is None:
-            raise ValueError("Please set the ANTHROPIC_API_KEY environment variable if you'd like to use Anthropic models.")
-        
+            raise ValueError(
+                "Please set the ANTHROPIC_API_KEY environment variable if you'd like to use Anthropic models."
+            )
+
         self.client = Anthropic(
             api_key=os.getenv("ANTHROPIC_API_KEY"),
         )
@@ -38,24 +46,42 @@ class ChatAnthropic(EngineLM, CachedEngine):
         self.system_prompt = system_prompt
         assert isinstance(self.system_prompt, str)
         self.is_multimodal = is_multimodal
+        self.tools = tools
+        self.tool_choice = tool_choice
 
     def __call__(self, prompt, **kwargs):
         return self.generate(prompt, **kwargs)
-    
+
     @retry(wait=wait_random_exponential(min=1, max=5), stop=stop_after_attempt(5))
-    def generate(self, content: Union[str, List[Union[str, bytes]]], system_prompt: str=None, **kwargs):
+    def generate(
+        self,
+        content: Union[str, List[Union[str, bytes]]],
+        system_prompt: str = None,
+        **kwargs,
+    ):
         if isinstance(content, str):
-            return self._generate_from_single_prompt(content, system_prompt=system_prompt, **kwargs)
-        
+            return self._generate_from_single_prompt(
+                content, system_prompt=system_prompt, **kwargs
+            )
+
         elif isinstance(content, list):
             has_multimodal_input = any(isinstance(item, bytes) for item in content)
             if (has_multimodal_input) and (not self.is_multimodal):
-                raise NotImplementedError("Multimodal generation is only supported for Claude-3 and beyond.")
-            
-            return self._generate_from_multiple_input(content, system_prompt=system_prompt, **kwargs)
+                raise NotImplementedError(
+                    "Multimodal generation is only supported for Claude-3 and beyond."
+                )
+
+            return self._generate_from_multiple_input(
+                content, system_prompt=system_prompt, **kwargs
+            )
 
     def _generate_from_single_prompt(
-        self, prompt: str, system_prompt: str=None, temperature=0, max_tokens=2000, top_p=0.99
+        self,
+        prompt: str,
+        system_prompt: str = None,
+        temperature=0,
+        max_tokens=2000,
+        top_p=0.99,
     ):
 
         sys_prompt_arg = system_prompt if system_prompt else self.system_prompt
@@ -88,26 +114,30 @@ class ChatAnthropic(EngineLM, CachedEngine):
                 image_type = get_image_type_from_bytes(item)
 
                 image_media_type = f"image/{image_type}"
-                base64_image = base64.b64encode(item).decode('utf-8')
-                formatted_content.append({
-                    "type": "image",
-                    "source": {
-                        "type": "base64",
-                        "media_type": image_media_type,
-                        "data": base64_image,
-                    },
-                })
+                base64_image = base64.b64encode(item).decode("utf-8")
+                formatted_content.append(
+                    {
+                        "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": image_media_type,
+                            "data": base64_image,
+                        },
+                    }
+                )
             elif isinstance(item, str):
-                formatted_content.append({
-                    "type": "text",
-                    "text": item
-                })
+                formatted_content.append({"type": "text", "text": item})
             else:
                 raise ValueError(f"Unsupported input type: {type(item)}")
         return formatted_content
 
     def _generate_from_multiple_input(
-        self, content: List[Union[str, bytes]], system_prompt=None, temperature=0, max_tokens=2000, top_p=0.99
+        self,
+        content: List[Union[str, bytes]],
+        system_prompt=None,
+        temperature=0,
+        max_tokens=2000,
+        top_p=0.99,
     ):
         sys_prompt_arg = system_prompt if system_prompt else self.system_prompt
         formatted_content = self._format_content(content)
@@ -125,7 +155,9 @@ class ChatAnthropic(EngineLM, CachedEngine):
             temperature=temperature,
             max_tokens=max_tokens,
             top_p=top_p,
-            system=sys_prompt_arg
+            system=sys_prompt_arg,
+            tools=self.tools,
+            tool_choice=self.tool_choice,
         )
 
         response_text = response.content[0].text
